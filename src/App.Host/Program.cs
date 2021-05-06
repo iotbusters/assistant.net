37b34dc5d4c462188cd7cfcef6278ee5a3818ad9
small <smallnonsense@gmail.com>
2021-05-06 18:53:39 +0300

    renamed host project

---
+++ src/App.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assistant.Net.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
