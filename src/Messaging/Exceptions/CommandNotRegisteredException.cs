e382f30ef843a79cace10abfee375655dc6626c6
small <smallnonsense@gmail.com>
2021-05-13 07:56:15 +0300

    added some annotations to excpetions

--- src/Messaging/Exceptions/CommandNotRegisteredException.cs
+++ src/Messaging/Exceptions/CommandNotRegisteredException.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Assistant.Net.Messaging.Exceptions
 {
+    /// <summary>
+    ///     No handlers were registered for command type exception.
+    /// </summary>
     public class CommandNotRegisteredException : CommandException
     {
         public CommandNotRegisteredException(string message) : base(message) { }
--- src/Messaging/Exceptions/UnknownCommandException.cs
+++ src/Messaging/Exceptions/UnknownCommandException.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Assistant.Net.Messaging.Exceptions
 {
+    /// <summary>
+    ///     A wrapper to unknown command exception returned by remote handler.
+    /// </summary>
     public sealed class UnknownCommandException : CommandException
     {
         public UnknownCommandException(string exceptionType, string? message, Exception? innerException)
