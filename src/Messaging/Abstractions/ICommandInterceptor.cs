771a629be8a9df94de3f870edf0dc7b440b24178
small <smallnonsense@gmail.com>
2021-05-13 07:11:06 +0300

    added annotations to messaging abstractions

--- src/Messaging/Abstractions/ICommandInterceptor.cs
+++ src/Messaging/Abstractions/ICommandInterceptor.cs
@@ -3,15 +3,31 @@ using System.Threading.Tasks;
 
 namespace Assistant.Net.Messaging.Abstractions
 {
+    /// <summary>
+    ///     Command interceptor abstraction that accepts <typeparamref name="TCommand" /> and its children.
+    ///     It's one piece in an intercepting chain with control over command and response.
+    /// </summary>
     public interface ICommandInterceptor<TCommand, TResponse> : IAbstractCommandInterceptor
         where TCommand : ICommand<TResponse>
     {
+        /// <summary>
+        ///     Intercepts the <paramref name="command" /> or one of its children
+        ///     and delegates the call to the <paramref name="next" /> interceptor if needed.
+        /// </summary>
         Task<TResponse> Intercept(TCommand command, Func<TCommand, Task<TResponse>> next);
     }
 
+    /// <summary>
+    ///     Command interceptor abstraction that accepts <typeparamref name="TCommand" /> and its children.
+    ///     It's one piece in an intercepting chain with control over command with no response expectation.
+    /// </summary>
     public interface ICommandInterceptor<TCommand> : ICommandInterceptor<TCommand, None>
         where TCommand : ICommand<None>
     {
+        /// <summary>
+        ///     Intercepts the <paramref name="command" /> or one of its children
+        ///     and delegates the call to the <paramref name="next" /> interceptor if needed.
+        /// </summary>
         Task Intercept(TCommand command, Func<TCommand, Task> next);
 
         async Task<None> ICommandInterceptor<TCommand, None>.Intercept(TCommand command, Func<TCommand, Task<None>> next)
@@ -21,10 +37,17 @@ namespace Assistant.Net.Messaging.Abstractions
         }
     }
 
+    /// <summary>
+    ///     Simple alias to interceptor that can handle all types of commands.
+    /// </summary>
     public interface ICommandInterceptor : ICommandInterceptor<ICommand<object>, object>
     {
     }
 
+    /// <summary>
+    ///     Very generic interceptor abstraction used primarily for type restrictions
+    ///     in configuration and other internal logic.
+    /// </summary>
     public interface IAbstractCommandInterceptor
     {
     }
--- src/Messaging/Abstractions/IHandlerFactory.cs
+++ src/Messaging/Abstractions/IHandlerFactory.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace Assistant.Net.Messaging.Abstractions
 {
+    /// <summary>
+    ///     Command handler resolver abstraction.
+    /// </summary>
     public interface IHandlerFactory
     {
+        /// <summary>
+        ///     Creates new instance of command handler by <paramref name="commandType" />.
+        /// </summary>
         IAbstractHandler Create(Type commandType);
     }
 }
