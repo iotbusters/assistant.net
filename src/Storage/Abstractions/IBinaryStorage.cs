e27a064680b1c3e314a2831c45bac6e843638e8d
small <smallnonsense@gmail.com>
2021-05-25 17:59:20 +0300

    added disposing to storages

--- src/Storage/Abstractions/IBinaryStorage.cs
+++ src/Storage/Abstractions/IBinaryStorage.cs
@@ -1,7 +1,9 @@
+using System;
+
 namespace Assistant.Net.Storage.Abstractions
 {
     /// <summary>
     ///    String key and binary value based common-purpose storage.
     /// </summary>
-    public interface IBinaryStorage : IStorage<string, byte[]> { }
+    public interface IBinaryStorage : IStorage<string, byte[]>, IDisposable { }
 }
