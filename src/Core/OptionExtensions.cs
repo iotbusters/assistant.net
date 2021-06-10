832b893a8f1de9991c54f8b0797c65f5883c51b5
small <smallnonsense@gmail.com>
2021-06-10 23:53:26 +0300

    extended core with additional tools

---
+++ src/Core/OptionExtensions.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Assistant.Net.Unions;
+
+namespace Assistant.Net
+{
+    public static class OptionExtensions
+    {
+        public static Task<Option<TResult>> Map<TSource, TResult>(this Task<Option<TSource>> source, Func<TSource, TResult> mapper) => TaskExtensions.Map(source, x => Map(x, mapper));
+
+        public static Option<TResult> Map<TSource, TResult>(this Option<TSource> option, Func<TSource, TResult> mapper) => option switch
+        {
+            Some<TSource>(var value)    => Option.Some(mapper(value)),
+            _                           => Option.None
+        };
+
+        public static Task<TSource?> GetValueOrDefault<TSource>(this Task<Option<TSource>> source) => TaskExtensions.Map(source, GetValueOrDefault);
+
+        public static TSource? GetValueOrDefault<TSource>(this Option<TSource> option) => option switch
+        {
+            Some<TSource>(var value)    => value,
+            _                           => default
+        };
+
+        public static TSource GetValueOrFail<TSource>(this Option<TSource> option) => option switch
+        {
+            Some<TSource>(var value)    => value,
+            _                           => throw new ArgumentException("Expected Some value only.")
+        };
+
+        public static bool TryGetValue<T>(this Option<T> option, out T? result)
+        {
+            if (option is Some<T>(var value))
+            {
+                result = value;
+                return true;
+            };
+            result = default;
+            return false;
+        }
+    }
+}
---
+++ src/Core/Serialization/ExceptionJsonConverter.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Assistant.Net.Abstractions;
+
+namespace Assistant.Net.Messaging.Serialization
+{
+    /// <summary>
+    ///     Json converter responsible for exception serialization.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-converters-how-to?pivots=dotnet-5-0"/> />
+    public class ExceptionJsonConverter<TValue> : JsonConverter<TValue>
+        where TValue : Exception
+    {
+        private const string TypePropertyName = "type";
+        private const string MessagePropertyName = "message";
+        private const string InnerExceptionPropertyName = "inner";
+
+        private readonly ITypeEncoder typeEncoder;
+
+        public ExceptionJsonConverter(ITypeEncoder typeEncoder) =>
+            this.typeEncoder = typeEncoder;
+
+        public override bool CanConvert(Type typeToConvert) =>
+            typeof(TValue).IsAssignableFrom(typeToConvert);
+
+        public override void Write(Utf8JsonWriter writer, TValue value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            writer.WriteString(TypePropertyName, GetType(value));
+
+            writer.WriteString(MessagePropertyName, GetMessage(value));
+
+            var inner = GetInnerException(value);
+            if (inner != null)
+            {
+                writer.WritePropertyName(InnerExceptionPropertyName);
+                JsonSerializer.Serialize(writer, inner, options);
+            }
+
+            writer.WriteEndObject();
+        }
+
+        public override TValue? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException("Start object token is expected.");
+
+            var (encodedType, message, inner) = ReadExceptionContent(ref reader, options);
+
+            if (encodedType == null)
+                throw new JsonException($"Property '{TypePropertyName}' is required.");
+
+            if (message == null)
+                throw new JsonException($"Property '{MessagePropertyName}' is required.");
+
+            try
+            {
+                var type = typeEncoder.Decode(encodedType!);
+                var ctorArguments = new object?[] { message, inner }
+                    .Where(x => x != null).Select(x => x!).ToArray();
+                return (TValue)Activator.CreateInstance(type!, ctorArguments)!;
+            }
+            catch (Exception ex)
+            {
+                throw new JsonException($"Cannot restore exception object by '{encodedType}', '{message}' and '{inner}'.", ex);
+            }
+        }
+
+        protected virtual string GetType(TValue value) => typeEncoder.Encode(value.GetType());
+
+        protected virtual string GetMessage(TValue value) => value.Message;
+
+        protected virtual Exception? GetInnerException(TValue value) => value.InnerException;
+
+        private static (string? type, string? message, Exception? inner) ReadExceptionContent(
+            ref Utf8JsonReader reader,
+            JsonSerializerOptions options)
+        {
+            string? encodedType = null;
+            string? message = null;
+            Exception? inner = null;
+
+            for (reader.Read(); reader.TokenType != JsonTokenType.EndObject; reader.Read())
+            {
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                    throw new JsonException("Property name token is expected.");
+                var propertyName = reader.GetString();
+
+                reader.Read();
+                switch (propertyName)
+                {
+                    case TypePropertyName:
+                        if (reader.TokenType != JsonTokenType.String)
+                            throw new JsonException("String token is expected.");
+                        encodedType = reader.GetString();
+                        break;
+
+                    case MessagePropertyName:
+                        if (reader.TokenType != JsonTokenType.String)
+                            throw new JsonException("String token is expected.");
+                        message = reader.GetString();
+                        break;
+
+                    case InnerExceptionPropertyName:
+                        if (reader.TokenType != JsonTokenType.StartObject)
+                            throw new JsonException("Start object token is expected.");
+                        inner = JsonSerializer.Deserialize<Exception>(ref reader, options);
+                        break;
+
+                    //case StackTracePropertyName: throw new NotSupportedException();
+
+                    default:
+                        reader.Skip(); // ignore additional property values
+                        break;
+                }
+            }
+
+            return (encodedType, message, inner);
+        }
+    }
+}
