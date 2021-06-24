7e1b2055136625914a3f9ddc95dc270838ae175f
small <smallnonsense@gmail.com>
2021-06-24 07:46:19 +0300

    hide shared static json serialialization

--- src/Core/Utils/SerializationExtensions.cs
+++ src/Core/Utils/SerializationExtensions.cs
@@ -6,7 +6,7 @@ namespace Assistant.Net.Utils
     /// <summary>
     ///     Object serialization extensions.
     /// </summary>
-    public static class SerializationExtensions
+    internal static class SerializationExtensions
     {
         /// <summary>
         ///     Serializes <param name="value" /> to byte array by internal strategy.
@@ -18,17 +18,7 @@ namespace Assistant.Net.Utils
             return SerializeClass(value);
         }
 
-        /// <summary>
-        ///     Deserializes <param name="bytes" /> to <typeparamref name="T"/> by internal strategy.
-        /// </summary>
-        public static T Deserialize<T>(this byte[] bytes)
-        {
-            if (typeof(T).IsValueType)
-                return DeserializeStructure<T>(bytes);
-            return DeserializeClass<T>(bytes);
-        }
-
-        internal static byte[] SerializeStructure<T>(this T value)
+        private static byte[] SerializeStructure<T>(this T value)
         {
             int size = Marshal.SizeOf(typeof(T));
             byte[] bytes = new byte[size];
@@ -41,22 +31,7 @@ namespace Assistant.Net.Utils
             return bytes;
         }
 
-        internal static T DeserializeStructure<T>(this byte[] bytes)
-        {
-            var size = Marshal.SizeOf(typeof(T));
-            var pointer = Marshal.AllocHGlobal(size);
-
-            Marshal.Copy(bytes, 0, pointer, size);
-            var value = (T)Marshal.PtrToStructure(pointer,  typeof(T))!;
-            Marshal.FreeHGlobal(pointer);
-
-            return value;
-        }
-
-        internal static byte[] SerializeClass<T>(this T value) =>
+        private static byte[] SerializeClass<T>(this T value) =>
             JsonSerializer.SerializeToUtf8Bytes(value);
-
-        internal static T DeserializeClass<T>(this byte[] value) =>
-            JsonSerializer.Deserialize<T>(value)!;
     }
 }
