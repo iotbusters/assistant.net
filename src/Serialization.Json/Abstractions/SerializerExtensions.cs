7d9d3787d64b52b9d916114e8f4d1446819bb211
small <smallnonsense@gmail.com>
2021-06-28 19:35:54 +0300

    minor updates

--- src/Serialization.Json/Abstractions/SerializerExtensions.cs
+++ src/Serialization.Json/Abstractions/SerializerExtensions.cs
@@ -5,9 +5,6 @@ namespace Assistant.Net.Serialization.Abstractions
 {
     public static class SerializerExtensions
     {
-        public static ISerializer<TValue> Create<TValue>(this ISerializerFactory factory) =>
-            (ISerializer<TValue>) factory.Create(typeof(ISerializer<TValue>));
-
         public static async Task<byte[]> Serialize<TValue>(this ISerializer<TValue> serializer, TValue value)
         {
             var stream = new MemoryStream();
@@ -17,5 +14,8 @@ namespace Assistant.Net.Serialization.Abstractions
 
         public static Task<TValue> Deserialize<TValue>(this ISerializer<TValue> serializer, byte[] bytes) => serializer
             .Deserialize(new MemoryStream(bytes));
+
+        public static Task<TValue> Deserialize<TValue>(this ISerializer<object> serializer, byte[] bytes) => serializer
+            .Deserialize(new MemoryStream(bytes)).MapSuccess(x => (TValue) x);
     }
 }
