56a41ba44a27fd6f1aff8c0a51f90be8ddd5b14b
small <smallnonsense@gmail.com>
2021-07-25 22:47:13 +0300

    Added new implementation of dynamic proxy, removed old one

---
+++ src/Dynamics/Options/ProxyFactoryOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assistant.Net.Dynamics.Options
+{
+    /// <summary>
+    ///     Required proxy configuration.
+    /// </summary>
+    public class ProxyFactoryOptions
+    {
+        private readonly HashSet<Type> proxyTypes = new();
+
+        /// <summary>
+        ///     Registers the <typeparamref name="T"/> proxy type required to proxy.
+        /// </summary>
+        public void Add<T>() => Add(typeof(T));
+
+        /// <summary>
+        ///     Registers the <paramref name="proxyType"/> required to proxy.
+        /// </summary>
+        public void Add(Type proxyType)
+        {
+            if (!proxyType.IsInterface)
+                throw new ArgumentException($"Expected an interface type but provided `{proxyType.Name}` instead.", nameof(proxyType));
+            proxyTypes.Add(proxyType);
+        }
+
+        /// <summary>
+        ///     Registered proxy types.
+        /// </summary>
+        internal Type[] ProxyTypes => proxyTypes.ToArray();
+    }
+}
---
+++ src/Dynamics/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Assistant.Net.Dynamics.Internal;
+using Assistant.Net.Dynamics.Options;
+using Assistant.Net.Dynamics.ProxyAnalyzer.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Assistant.Net.Dynamics
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Adds <see cref="IProxyFactory" /> implementation.
+        ///     Pay attention, you may need to call explicitly <see cref="ConfigureProxyFactoryOptions" />
+        ///     to ensure required proxy types are registered.
+        /// </summary>
+        public static IServiceCollection AddProxyFactory(this IServiceCollection services) => services
+            .TryAddSingleton<IProxyFactory, ProxyFactory>();
+
+        /// <summary>
+        ///     Adds <see cref="IProxyFactory"/> implementation and <see cref="ProxyFactoryOptions"/> configuration.
+        /// </summary>
+        public static IServiceCollection AddProxyFactory(this IServiceCollection services, Action<ProxyFactoryOptions> configureOptions) => services
+            .ConfigureProxyFactoryOptions(configureOptions)
+            .AddProxyFactory();
+
+        /// <summary>
+        ///     Register an action used to configure <see cref="ProxyFactoryOptions"/> options.
+        /// </summary>
+        public static IServiceCollection ConfigureProxyFactoryOptions(this IServiceCollection services, Action<ProxyFactoryOptions> configureOptions) => services
+            .Configure(configureOptions);
+    }
+}
