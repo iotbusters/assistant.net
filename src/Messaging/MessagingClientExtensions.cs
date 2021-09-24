dfa62ef0381ded1e564062bdb6cd1e61eaeb1166
small <smallnonsense@gmail.com>
2021-09-24 21:37:56 +0300

    added missed token to messaging client extensions

--- src/Messaging/MessagingClientExtensions.cs
+++ src/Messaging/MessagingClientExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Assistant.Net.Messaging.Abstractions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Assistant.Net.Messaging
@@ -12,13 +13,14 @@ namespace Assistant.Net.Messaging
         ///     Sends asynchronously a request to associated request handler expecting a specific object in respond.
         /// </summary>
         /// <typeparam name="TResponse">Response object type.</typeparam>
-        public static Task<TResponse> SendAs<TResponse>(this IMessagingClient client, IMessage<TResponse> message) =>
-            client.Send(message).MapCompleted(x => (TResponse) x);
+        public static Task<TResponse> SendAs<TResponse>(this IMessagingClient client, IMessage<TResponse> message, CancellationToken token = default) =>
+            client.Send(message, token).MapCompleted(x => (TResponse) x);
 
         /// <summary>
         ///     Sends asynchronously a request to associated request handler.
         ///     Similar to request although in opposite expecting successful execution only.
         /// </summary>
-        public static Task SendAs(this IMessagingClient client, IMessage message) => client.SendAs<None>(message);
+        public static Task SendAs(this IMessagingClient client, IMessage message, CancellationToken token = default) =>
+            client.SendAs<None>(message, token);
     }
 }
