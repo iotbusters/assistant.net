77339b5fd625952b4c79aba27f52a2e6c7cb4f37
small <smallnonsense@gmail.com>
2021-10-11 20:31:07 +0300

    added mongo client implementation for messaging

---
+++ src/Messaging.Mongo.Client/Internal/MongoMessageHandlerProxy.cs
@@ -0,0 +1,108 @@
+﻿using Assistant.Net.Abstractions;
+using Assistant.Net.Diagnostics.Abstractions;
+using Assistant.Net.Messaging.Abstractions;
+using Assistant.Net.Messaging.Exceptions;
+using Assistant.Net.Messaging.Models;
+using Assistant.Net.Messaging.Options;
+using Assistant.Net.Unions;
+using Assistant.Net.Utils;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assistant.Net.Messaging.Internal
+{
+    internal class MongoMessageHandlerProxy<TMessage, TResponse> : IMessageHandler<TMessage, TResponse>
+        where TMessage : IMessage<TResponse>
+    {
+        private readonly ILogger logger;
+        private readonly IDiagnosticContext context;
+        private readonly MongoHandlingClientOptions options;
+        private readonly IMongoCollection<MongoRecord> collection;
+        private readonly ISystemClock clock;
+
+        public MongoMessageHandlerProxy(
+            ILogger<MongoMessageHandlerProxy<TMessage, TResponse>> logger,
+            IOptions<MongoHandlingClientOptions> options,
+            IDiagnosticContext context,
+            IMongoClient client,
+            ISystemClock clock)
+        {
+            this.logger = logger;
+            this.context = context;
+            this.clock = clock;
+            this.options = options.Value;
+            this.collection = client.GetDatabase(this.options.DatabaseName).GetCollection<MongoRecord>(MongoNames.MessageCollectionName);
+        }
+
+        public async Task<TResponse> Handle(TMessage message, CancellationToken token)
+        {
+            var strategy = options.ResponsePoll;
+
+            var attempt = 1;
+
+            var requested = MongoRecord.Request(message.GetSha1(), message, context.CorrelationId, clock.UtcNow);
+            if (await TryInsertOne(requested, token))
+                await Task.Delay(strategy.DelayTime(attempt), token);
+
+            while (true)
+            {
+                logger.LogDebug("Message({MessageName}/{MessageId}) polling: {Attempt} begins.", requested.Name, requested.Id, attempt);
+
+                if (await FindOneResponded(requested.Id, token) is Some<MongoRecord>(var responded))
+                {
+                    logger.LogInformation("Message({MessageName}/{MessageId}) polling: {Attempt} ends with {status}.", requested.Name, requested.Id, attempt, responded.Status);
+
+                    if (responded.Status == HandlingStatus.Succeeded)
+                        return (TResponse)responded.Response!;
+
+                    if (responded.Status == HandlingStatus.Failed)
+                        ((Exception)responded.Response!).Throw();
+
+                    throw new MessageContractException($"Not expected status {responded.Status}.");
+                }
+
+                logger.LogInformation("Message({MessageName}/{MessageId}) polling: {Attempt} ends without response.", requested.Name, requested.Id, attempt);
+
+                attempt++;
+                if (!strategy.CanRetry(attempt))
+                {
+                    logger.LogInformation("Message({MessageName}/{MessageId}) polling: {Attempt} won't proceed.", requested.Name, requested.Id, attempt);
+                    break;
+                }
+
+                await Task.Delay(strategy.DelayTime(attempt), token);
+            }
+
+            throw new MessageDeferredException();
+        }
+
+        private async Task<bool> TryInsertOne(MongoRecord record, CancellationToken token)
+        {
+            try
+            {
+                await collection.InsertOneAsync(
+                    record,
+                    new InsertOneOptions(),
+                    token);
+                return true;
+            }
+            catch (MongoWriteException ex) when (ex.WriteError.Category == ServerErrorCategory.DuplicateKey)
+            {
+                logger.LogDebug("Message({MessageName}/{MessageId}) polling: already requested.", record.Name, record.Id);
+                return false;
+            }
+        }
+
+        private async Task<Option<MongoRecord>> FindOneResponded(string id, CancellationToken token)
+        {
+            var existed = await collection
+                .Find(filter: x => x.Id == id && x.Status != HandlingStatus.Requested, new FindOptions())
+                .SingleOrDefaultAsync(token);
+            return existed.AsOption();
+        }
+    }
+}
---
+++ src/Messaging.Mongo.Client/Options/MongoHandlingClientOptions.cs
@@ -0,0 +1,24 @@
+using Assistant.Net.Abstractions;
+using Assistant.Net.RetryStrategies;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assistant.Net.Messaging.Options
+{
+    /// <summary>
+    ///     MongoDB client configurations used for remote message handling coordination.
+    /// </summary>
+    public class MongoHandlingClientOptions
+    {
+        /// <summary>
+        ///     Database name.
+        /// </summary>
+        [Required]
+        public string DatabaseName { get; set; } = MongoNames.DatabaseName;
+
+        /// <summary>
+        ///     Message handling response polling strategy.
+        /// </summary>
+        public IRetryStrategy ResponsePoll { get; set; } = new ConstantBackoff {MaxAttemptNumber = 4, Interval = TimeSpan.FromSeconds(0.1)};
+    }
+}
