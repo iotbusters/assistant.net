8b9dcb6e77a82b29d8e15f7219727023f3d97cc4
small <smallnonsense@gmail.com>
2021-10-11 08:07:22 +0300

    introduced retry strategy to storage, moved this configuration into separate options

---
+++ src/Storage.Mongo/MongoNames.cs
@@ -0,0 +1,33 @@
+﻿namespace Assistant.Net.Storage
+{
+    /// <summary>
+    ///     MongoDB related resource names for data storing.
+    /// </summary>
+    public class MongoNames
+    {
+        /// <summary>
+        ///     Database name.
+        /// </summary>
+        public const string DatabaseName = "Storage";
+
+        /// <summary>
+        ///     Storage collection name.
+        /// </summary>
+        public const string StorageCollectionName = "Records";
+
+        /// <summary>
+        ///     Partitioned storage key collection name.
+        /// </summary>
+        public const string PartitionStorageKeyCollectionName = "PartitionKeys";
+
+        /// <summary>
+        ///     Partitioned storage key/value relation collection name.
+        /// </summary>
+        public const string PartitionStorageKeyValueCollectionName = "PartitionIndexes";
+
+        /// <summary>
+        ///     Partitioned storage value collection name.
+        /// </summary>
+        public const string PartitionStorageValueCollectionName = "PartitionRecords";
+    }
+}
