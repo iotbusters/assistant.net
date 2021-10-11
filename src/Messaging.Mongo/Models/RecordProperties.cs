c153a18b852010411d6d0a32bb6c88d042b7e2ad
small <smallnonsense@gmail.com>
2021-10-11 20:30:20 +0300

    added basic mongo related classes for messaging

---
+++ src/Messaging.Mongo/Models/RecordProperties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Assistant.Net.Messaging.Models
+{
+    /// <summary>
+    ///     
+    /// </summary>
+    public class RecordProperties
+    {
+        /// <summary/>
+        public RecordProperties(string correlationId, DateTimeOffset created, DateTimeOffset? updated)
+        {
+            CorrelationId = correlationId;
+            Created = created;
+            Updated = updated;
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        public string CorrelationId { get; private set; }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        public DateTimeOffset Created { get; private set; }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        public DateTimeOffset? Updated { get; private set; }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        public static RecordProperties OnCreated(string correlationId, DateTimeOffset created) => new(correlationId, created, updated: null);
+
+        /// <summary>
+        ///     
+        /// </summary>
+        public RecordProperties OnUpdated(DateTimeOffset updated) => new(CorrelationId, Created, updated);
+    }
+}
