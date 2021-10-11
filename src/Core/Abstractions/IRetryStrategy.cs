e751ec97ddbe4388db26837e9eb1d7f7e8361f91
small <smallnonsense@gmail.com>
2021-10-11 07:41:09 +0300

    moved retrying strategy into core package

---
+++ src/Core/Abstractions/IRetryStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assistant.Net.Abstractions
+{
+    /// <summary>
+    ///     Message handling retry strategy.
+    /// </summary>
+    public interface IRetryStrategy
+    {
+        /// <summary>
+        ///     Max allowed handling attempts before failure.
+        /// </summary>
+        [Required, Range(minimum: 1, maximum: 500)]
+        public int? MaxAttemptNumber { get; set; }
+
+        /// <summary>
+        ///     Determines if the attempt <paramref name="attemptNumber"/> can be retried.
+        /// </summary>
+        public virtual bool CanRetry(int attemptNumber) =>
+            attemptNumber <= MaxAttemptNumber;
+
+        /// <summary>
+        ///     Time to delay before next handling attempt.
+        /// </summary>
+        TimeSpan DelayTime(int attemptNumber);
+    }
+}
---
+++ src/Core/RetryStrategies/ConstantBackoff.cs
@@ -0,0 +1,23 @@
+﻿using Assistant.Net.Abstractions;
+using System;
+
+namespace Assistant.Net.RetryStrategies
+{
+    /// <summary>
+    ///     Constant backoff implementation of retrying strategy.
+    /// </summary>
+    public class ConstantBackoff : IRetryStrategy
+    {
+        /// <inheritdoc/>
+        public int? MaxAttemptNumber { get; set; }
+
+        /// <summary>
+        ///     Constant interval time.
+        /// </summary>
+        /// [Required]
+        public TimeSpan? Interval { get; set; }
+
+        /// <inheritdoc/>
+        public TimeSpan DelayTime(int attemptNumber) => Interval!.Value;
+    }
+}
