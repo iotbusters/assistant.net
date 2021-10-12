6900060be6533559e52fc1b317c16e13a1d854a2
small <smallnonsense@gmail.com>
2021-10-12 16:36:41 +0300

    updated tests, added concurrency testing for mongo.

--- tests/Messaging.Web.Tests/Fixtures/MessageClientFixture.cs
+++ tests/Messaging.Web.Tests/Fixtures/MessageClientFixture.cs
@@ -1,20 +1,27 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
 using Assistant.Net.Messaging.Abstractions;
+using Microsoft.Extensions.Hosting;
 
 namespace Assistant.Net.Messaging.Web.Tests.Fixtures
 {
     public class MessageClientFixture : IDisposable
     {
         private readonly ServiceProvider provider;
+        private readonly IHost host;
 
-        public MessageClientFixture(ServiceProvider provider)
+        public MessageClientFixture(ServiceProvider provider, IHost host)
         {
             this.provider = provider;
+            this.host = host;
         }
 
         public IMessagingClient Client => provider.GetRequiredService<IMessagingClient>();
 
-        public virtual void Dispose() => provider.Dispose();
+        public virtual void Dispose()
+        {
+            provider.Dispose();
+            host.Dispose();
+        }
     }
 }
