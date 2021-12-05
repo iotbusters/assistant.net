bd9826084d8616597ccfa40223c887f7065c478f
small <smallnonsense@gmail.com>
2021-12-05 15:03:21 +0200

    Introduces options source concept as alternative to regular configuration.

---
+++ src/Core/Abstractions/ConfigureOptionsSourceBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System.Threading;
+
+namespace Assistant.Net.Abstractions
+{
+    /// <summary>
+    ///     Base implementation of configure options source.
+    /// </summary>
+    public abstract class ConfigureOptionsSourceBase<TOptions> : IConfigureOptionsSource<TOptions>
+        where TOptions : class
+    {
+        private ConfigurationReloadToken changeToken = new();
+
+        /// <inheritdoc/>
+        public IChangeToken GetChangeToken() => changeToken;
+
+        /// <inheritdoc/>
+        public abstract void Configure(TOptions options);
+
+        /// <summary>
+        ///     Triggers the change token reload.
+        /// </summary>
+        public void Reload() => Interlocked.Exchange(ref changeToken, new()).OnReload();
+    }
+}
