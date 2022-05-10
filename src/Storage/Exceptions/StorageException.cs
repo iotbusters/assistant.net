290cb8f211f106c877d0b487bd17ea0634403950
small <smallnonsense@gmail.com>
2022-05-10 21:15:17 +0300

    updated storage - wrapped unexpected exceptions - added index/version argument validation

---
+++ src/Storage/Exceptions/StorageException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assistant.Net.Storage.Exceptions;
+
+/// <summary>
+///     Generic storage exception.
+/// </summary>
+public class StorageException : Exception
+{
+    private const string DefaultMessage = "Storage operation has failed.";
+
+    /// <summary />
+    public StorageException() : this(DefaultMessage) { }
+
+    /// <summary />
+    public StorageException(string? message) : base(message ?? DefaultMessage) { }
+
+    /// <summary />
+    public StorageException(string? message, Exception? ex) : base(message ?? DefaultMessage, ex) { }
+
+    /// <summary />
+    public StorageException(Exception ex) : base(DefaultMessage, ex) { }
+}
