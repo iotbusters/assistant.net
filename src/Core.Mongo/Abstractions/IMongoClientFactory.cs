d09c9eb04fe60734796dc2ea5c053e5a50b75909
small <smallnonsense@gmail.com>
2022-05-17 17:54:14 +0300

    Moved mongo db related classes into a separate project

---
+++ src/Core.Mongo/Abstractions/IMongoClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+
+namespace Assistant.Net.Abstractions
+{
+    /// <summary>
+    ///     MongoDB client factory for storage provider.
+    /// </summary>
+    public interface IMongoClientFactory
+    {
+        /// <summary>
+        ///     Creates MongoDB client instance.
+        /// </summary>
+        /// <param name="name">Client configuration name.</param>
+        IMongoClient CreateClient(string name);
+
+        /// <summary>
+        ///     Gets MongoDB client's database instance.
+        /// </summary>
+        /// <param name="name">Client configuration name.</param>
+        IMongoDatabase GetDatabase(string name);
+    }
+}
