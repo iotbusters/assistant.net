190811a382acff6f32ed0f4c36838f9ddfb05532
small <smallnonsense@gmail.com>
2022-06-04 21:11:35 +0300

    introduced abstract builder and isolated mongo/sqlite  provider configuration

---
+++ src/Core.Sqlite/BuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Assistant.Net.Abstractions;
+using Assistant.Net.Options;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Assistant.Net;
+
+/// <summary>
+///     Builder marker extensions for SQLite provider.
+/// </summary>
+public static class BuilderExtensions
+{
+    /// <summary>
+    ///     Configures the messaging client to connect a SQLite database from a client.
+    /// </summary>
+    public static TBuilder UseSqlite<TBuilder>(this IBuilder<TBuilder> builder, string connectionString) => builder
+        .UseSqlite(o => o.ConnectionString = connectionString);
+
+    /// <summary>
+    ///     Configures the messaging client to connect a SQLite database from a client.
+    /// </summary>
+    public static TBuilder UseSqlite<TBuilder>(this IBuilder<TBuilder> builder, SqliteConnection connection)
+    {
+        builder.Services.ConfigureSqliteOptions(builder.Name, o => o.Connection(connection));
+        return builder.Instance;
+    }
+
+    /// <summary>
+    ///     Configures the messaging client to connect a SQLite database from a client.
+    /// </summary>
+    public static TBuilder UseSqlite<TBuilder>(this IBuilder<TBuilder> builder, Action<SqliteOptions> configureOptions)
+    {
+        builder.Services.ConfigureSqliteOptions(builder.Name, configureOptions);
+        return builder.Instance;
+    }
+
+    /// <summary>
+    ///     Configures the messaging client to connect a SQLite database from a client.
+    /// </summary>
+    public static TBuilder UseSqlite<TBuilder>(this IBuilder<TBuilder> builder, IConfigurationSection configuration)
+    {
+        builder.Services.ConfigureSqliteOptions(builder.Name, configuration);
+        return builder.Instance;
+    }
+}
