012f98fd37fd55ddca96e454abc0a359731912bd
small <smallnonsense@gmail.com>
2022-06-17 20:49:10 +0300

    added scoped tests for options change on feature

--- tests/Core.Tests/ConfigureOptionsSourceTests.cs
+++ tests/Core.Tests/ConfigureOptionsSourceTests.cs
@@ -9,9 +9,9 @@ namespace Assistant.Net.Core.Tests;
 public class ConfigureOptionsSourceTests
 {
     [Test]
-    public void ChangeOn_notSynchronizesOptions_boundToAnotherNamedOptions()
+    public void ChangeOn_notSynchronizesOptionsMonitor_boundToAnotherNamedOptions()
     {
-        var source = new TestConfigureOptionsSource<TestOptions2> {ConfigureAction = o => o.Value = "1"};
+        var source = new TestConfigureOptionsSource<TestOptions2> { ConfigureAction = o => o.Value = "1" };
         var provider = new ServiceCollection()
             .AddOptions<TestOptions1>("name-1")
             .ChangeOn<TestOptions2>("name-2")
@@ -31,9 +31,9 @@ public class ConfigureOptionsSourceTests
     }
 
     [Test]
-    public void ChangeOn_synchronizesOptions_onInit()
+    public void ChangeOn_synchronizesOptionsMonitor_onInit()
     {
-        var source = new TestConfigureOptionsSource<TestOptions2> {ConfigureAction = o => o.Value = "1"};
+        var source = new TestConfigureOptionsSource<TestOptions2> { ConfigureAction = o => o.Value = "1" };
         var provider = new ServiceCollection()
             .AddOptions<TestOptions1>("name-1")
             .ChangeOn<TestOptions2>("name-2")
@@ -52,9 +52,9 @@ public class ConfigureOptionsSourceTests
     }
 
     [Test]
-    public void ChangeOn_synchronizesOptions_onReload()
+    public void ChangeOn_synchronizesOptionsMonitor_onReload()
     {
-        var source = new TestConfigureOptionsSource<TestOptions2> {ConfigureAction = o => o.Value = "1"};
+        var source = new TestConfigureOptionsSource<TestOptions2> { ConfigureAction = o => o.Value = "1" };
         var provider = new ServiceCollection()
             .AddOptions<TestOptions1>("name-1")
             .ChangeOn<TestOptions2>("name-2")
@@ -67,6 +67,76 @@ public class ConfigureOptionsSourceTests
 
         var options1 = provider.GetRequiredService<IOptionsMonitor<TestOptions1>>();
         var options2 = provider.GetRequiredService<IOptionsMonitor<TestOptions2>>();
+
+        options1.Get("name-1").Value.Should().Be("1");
+        options2.Get("name-2").Value.Should().Be("1");
+
+        source.ConfigureAction = o => o.Value = "2";
+        source.Reload();
+
+        options1.Get("name-1").Value.Should().Be("2");
+        options2.Get("name-2").Value.Should().Be("2");
+    }
+
+    [Test]
+    public void ChangeOn_notSynchronizesOptionsSnapshot_boundToAnotherNamedOptions()
+    {
+        var source = new TestConfigureOptionsSource<TestOptions2> {ConfigureAction = o => o.Value = "1"};
+        var provider = new ServiceCollection()
+            .AddOptions<TestOptions1>("name-1")
+            .ChangeOn<TestOptions2>("name-2")
+            .Configure<IOptionsSnapshot<TestOptions2>>((o, m) => o.Value = m.Get("name-2").Value)
+            .Services
+            .AddOptions<TestOptions2>()
+            .Bind(source)
+            .Services
+            .BuildServiceProvider();
+
+        var options1 = provider.GetRequiredService<IOptionsSnapshot<TestOptions1>>();
+        var options2 = provider.GetRequiredService<IOptionsSnapshot<TestOptions2>>();
+
+        options1.Get("name-1").Value.Should().BeNull();
+        options2.Get("name-2").Value.Should().BeNull();
+        options2.Get("").Value.Should().Be("1");
+    }
+
+    [Test]
+    public void ChangeOn_synchronizesOptionsSnapshot_onInit()
+    {
+        var source = new TestConfigureOptionsSource<TestOptions2> {ConfigureAction = o => o.Value = "1"};
+        var provider = new ServiceCollection()
+            .AddOptions<TestOptions1>("name-1")
+            .ChangeOn<TestOptions2>("name-2")
+            .Configure<IOptionsSnapshot<TestOptions2>>((o, m) => o.Value = m.Get("name-2").Value)
+            .Services
+            .AddOptions<TestOptions2>("name-2")
+            .Bind(source)
+            .Services
+            .BuildServiceProvider();
+
+        var options1 = provider.GetRequiredService<IOptionsSnapshot<TestOptions1>>();
+        var options2 = provider.GetRequiredService<IOptionsSnapshot<TestOptions2>>();
+
+        options1.Get("name-1").Value.Should().Be("1");
+        options2.Get("name-2").Value.Should().Be("1");
+    }
+
+    [Test]
+    public void ChangeOn_synchronizesOptionsSnapshot_onReload()
+    {
+        var source = new TestConfigureOptionsSource<TestOptions2> {ConfigureAction = o => o.Value = "1"};
+        var provider = new ServiceCollection()
+            .AddOptions<TestOptions1>("name-1")
+            .ChangeOn<TestOptions2>("name-2")
+            .Configure<IOptionsSnapshot<TestOptions2>>((o, m) => o.Value = m.Get("name-2").Value)
+            .Services
+            .AddOptions<TestOptions2>("name-2")
+            .Bind(source)
+            .Services
+            .BuildServiceProvider();
+
+        var options1 = provider.GetRequiredService<IOptionsSnapshot<TestOptions1>>();
+        var options2 = provider.GetRequiredService<IOptionsSnapshot<TestOptions2>>();
 
         options1.Get("name-1").Value.Should().Be("1");
         options2.Get("name-2").Value.Should().Be("1");
