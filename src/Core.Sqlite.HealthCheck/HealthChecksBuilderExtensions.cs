1d557f100c453c94825487dac98b4be985352a62
small <smallnonsense@gmail.com>
2022-08-07 22:38:15 +0300

    extended healthcheck registration methods

--- src/Core.Sqlite.HealthCheck/HealthChecksBuilderExtensions.cs
+++ src/Core.Sqlite.HealthCheck/HealthChecksBuilderExtensions.cs
@@ -3,6 +3,7 @@ using Assistant.Net.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
+using System.Linq;
 
 namespace Assistant.Net;
 
@@ -15,5 +16,38 @@ public static class HealthChecksBuilderExtensions
     ///     Adds a <see cref="SqliteOptions"/> configuration based health check implementation.
     /// </summary>
     public static IHealthChecksBuilder AddSqlite(this IHealthChecksBuilder builder, TimeSpan? timeout = null) => builder
-        .AddCheck<SqliteOptionsHealthCheck>(nameof(SqliteOptions), HealthStatus.Unhealthy, timeout: timeout);
+        .AddSqlite(nameof(SqliteOptions), timeout);
+
+    /// <summary>
+    ///     Adds a <see cref="SqliteOptions"/> configuration based health check implementation.
+    /// </summary>
+    public static IHealthChecksBuilder AddSqlite(this IHealthChecksBuilder builder, string name, TimeSpan? timeout = null) => builder
+        .AddCheck<SqliteOptionsHealthCheck>(name, HealthStatus.Unhealthy, tags: null, timeout: timeout);
+
+    /// <summary>
+    ///     Replace a <see cref="SqliteOptions"/> configuration based health check implementation.
+    /// </summary>
+    public static IHealthChecksBuilder ReplaceSqlite(this IHealthChecksBuilder builder, TimeSpan? timeout = null) => builder
+        .ReplaceSqlite(nameof(SqliteOptions), timeout);
+
+    /// <summary>
+    ///     Replace a <see cref="SqliteOptions"/> configuration based health check implementation.
+    /// </summary>
+    public static IHealthChecksBuilder ReplaceSqlite(this IHealthChecksBuilder builder, string name, TimeSpan? timeout = null)
+    {
+        builder.Services.Configure<HealthCheckServiceOptions>(options =>
+        {
+            var registration = options.Registrations.FirstOrDefault(x => x.Name == name);
+            if (registration != null)
+                options.Registrations.Remove(registration);
+
+            options.Registrations.Add(new HealthCheckRegistration(
+                name,
+                factory: p => ActivatorUtilities.CreateInstance<SqliteOptionsHealthCheck>(p),
+                failureStatus: HealthStatus.Unhealthy,
+                tags: null,
+                timeout));
+        });
+        return builder;
+    }
 }
