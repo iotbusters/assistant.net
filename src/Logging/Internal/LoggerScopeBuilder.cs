814ff120d0326164e14d1cfc2db244b3e999e120
small <smallnonsense@gmail.com>
2022-09-03 20:00:58 +0300

    added global/runtime logging scopes

---
+++ src/Logging/Internal/LoggerScopeBuilder.cs
@@ -0,0 +1,40 @@
+﻿using Assistant.Net.Abstractions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Assistant.Net.Internal;
+
+/// <summary>
+///     Logging scope builder implementation.
+/// </summary>
+internal class LoggerScopeBuilder : ILoggerScopeBuilder
+{
+    private readonly Dictionary<string, object> properties = new();
+    private readonly IDisposable scope;
+
+    /// <summary/>
+    public LoggerScopeBuilder(ILogger logger) =>
+        this.scope = logger.BeginScope(properties);
+
+    /// <inheritdoc/>
+    public ILoggerScopeBuilder AddPropertyScope(string name, object value)
+    {
+        var key = name ?? throw new ArgumentNullException(nameof(name));
+        properties[key] = value ?? throw new ArgumentNullException(nameof(value));
+
+        return this;
+    }
+
+    /// <inheritdoc/>
+    public ILoggerScopeBuilder AddPropertyScope(string name, Func<object> valueFactory) =>
+        AddPropertyScope(name, (object)valueFactory);
+
+    /// <inheritdoc/>
+    public ILoggerScopeBuilder AddPropertyScope(string name, Func<IServiceProvider, object> valueFactory) =>
+        AddPropertyScope(name, (object)valueFactory);
+
+    /// <inheritdoc />
+    public void Dispose() =>
+        scope.Dispose();
+}
---
+++ src/Logging/LoggingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Assistant.Net.Abstractions;
+using Assistant.Net.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Assistant.Net;
+
+/// <summary>
+///     Logger configuring extensions.
+/// </summary>
+public static class LoggingExtensions
+{
+    /// <summary>
+    ///     Begins logging property scope.
+    /// </summary>
+    public static ILoggerScopeBuilder BeginPropertyScope(this ILogger logger) =>
+        new LoggerScopeBuilder(logger);
+
+    /// <summary>
+    ///     Begins logging property scope with a property.
+    /// </summary>
+    /// <param name="logger"/>
+    /// <param name="name">Property name.</param>
+    /// <param name="value">Property value.</param>
+    public static ILoggerScopeBuilder BeginPropertyScope(this ILogger logger, string name, object value) =>
+        logger.BeginPropertyScope().AddPropertyScope(name, value);
+
+    /// <summary>
+    ///     Begins logging property scope with a property.
+    /// </summary>
+    /// <param name="logger"/>
+    /// <param name="name">Property name.</param>
+    /// <param name="valueFactory">Property value factory.</param>
+    public static ILoggerScopeBuilder BeginPropertyScope(this ILogger logger, string name, Func<object> valueFactory) =>
+        logger.BeginPropertyScope().AddPropertyScope(name, valueFactory);
+
+    /// <summary>
+    ///     Begins logging property scope with a property.
+    /// </summary>
+    /// <param name="logger"/>
+    /// <param name="name">Property name.</param>
+    /// <param name="valueFactory">Property value factory.</param>
+    public static ILoggerScopeBuilder BeginPropertyScope(this ILogger logger, string name, Func<IServiceProvider, object> valueFactory) =>
+        logger.BeginPropertyScope().AddPropertyScope(name, valueFactory);
+}
