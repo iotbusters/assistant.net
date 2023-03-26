e3ecc60a85f81bbfc74a716c7404e779bce3485f
small <smallnonsense@gmail.com>
2023-03-26 18:18:45 +0300

    added caching by factory argument.

--- src/Core/Options/InstanceCachingFactory.cs
+++ src/Core/Options/InstanceCachingFactory.cs
@@ -1,34 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Assistant.Net.Options;
 
 /// <summary>
-///     Lazily initializing <typeparamref name="TInstance"/> instance factory with caching behavior.
+///     Lazily initializing <typeparamref name="TInstance" /> instance factory with caching behavior.
 /// </summary>
 /// <typeparam name="TInstance">Lazily creating type.</typeparam>
 public sealed class InstanceCachingFactory<TInstance> : InstanceFactory<TInstance> where TInstance : class?
 {
     private TInstance? instance;
 
-    /// <summary/>
+    /// <summary />
     public InstanceCachingFactory(Func<IServiceProvider, TInstance> factory) : base(factory) { }
 
-    /// <inheritdoc/>
-    public override TInstance Create(IServiceProvider provider) => instance ??= Factory(provider);
+    /// <inheritdoc />
+    public override TInstance Create(IServiceProvider provider)
+    {
+        return instance ??= Factory(provider);
+    }
 }
 
 /// <summary>
-///     Lazily initializing <typeparamref name="TInstance"/> instance factory with caching behavior.
+///     Lazily initializing <typeparamref name="TInstance" /> instance factory with caching behavior.
 /// </summary>
 /// <typeparam name="TInstance">Lazily creating type.</typeparam>
-/// <typeparam name="TParameter">Parameter type required for creating <typeparamref name="TInstance"/> type.</typeparam>
-public sealed class InstanceCachingFactory<TInstance, TParameter> : InstanceFactory<TInstance, TParameter> where TInstance : class?
+/// <typeparam name="TParameter">Parameter type required for creating <typeparamref name="TInstance" /> type.</typeparam>
+public sealed class InstanceCachingFactory<TInstance, TParameter> : InstanceFactory<TInstance, TParameter>
+    where TInstance : class?
+    where TParameter : notnull
 {
-    private TInstance? instance;
+    private readonly Dictionary<TParameter, TInstance> instances = new();
 
-    /// <summary/>
+    /// <summary />
     public InstanceCachingFactory(Func<IServiceProvider, TParameter, TInstance> factory) : base(factory) { }
 
-    /// <inheritdoc/>
-    public override TInstance Create(IServiceProvider provider, TParameter parameter) => instance ??= Factory(provider, parameter);
+    /// <inheritdoc />
+    public override TInstance Create(IServiceProvider provider, TParameter parameter)
+    {
+        if (instances.TryGetValue(parameter, out var instance))
+            return instance;
+        return instances[parameter] = Factory(provider, parameter);
+    }
 }
