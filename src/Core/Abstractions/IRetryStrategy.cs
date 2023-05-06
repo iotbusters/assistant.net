7ac600fd4af32f05b0cd2c723cf2b674575af370
small <smallnonsense@gmail.com>
2023-05-06 20:27:10 +0300

    moved retrying strategy creation to dedicated static method.

--- src/Core/Abstractions/IRetryStrategy.cs
+++ src/Core/Abstractions/IRetryStrategy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assistant.Net.RetryStrategies;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
@@ -10,6 +12,22 @@ namespace Assistant.Net.Abstractions;
 public interface IRetryStrategy
 {
     /// <summary>
+    ///     Reads configured backoff strategy.
+    /// </summary>
+    /// <remarks>
+    ///     For example: <c>{ type: Constant, maxAttemptNumber: 5, interval: 00:00:01 }</c>.
+    /// </remarks>
+    /// <exception cref="ArgumentException"></exception>
+    public static IRetryStrategy ReadStrategy(IConfigurationSection configuration) => configuration["type"] switch
+    {
+        "Exponential" => configuration.Get<ExponentialBackoff>()!,
+        "Linear" => configuration.Get<LinearBackoff>()!,
+        "Constant" => configuration.Get<ConstantBackoff>()!,
+        _ => throw new ArgumentException($"Key 'type' at {configuration.Path} is expected to be: "
+                                         + $"'Exponential', 'Linear' or 'Constant' but was '{configuration["type"]}'.")
+    };
+
+    /// <summary>
     ///     Total time of all delays.
     /// </summary>
     public TimeSpan TotalTime => Enumerable.Range(1, MaxAttemptNumber).Select(DelayTime).Aggregate((x, y) => x + y);
@@ -18,7 +36,7 @@ public interface IRetryStrategy
     ///     Max allowed handling attempts before failure.
     /// </summary>
     [Required, Range(minimum: 1, maximum: 1000)]
-    public int MaxAttemptNumber { get; set; }
+    public int MaxAttemptNumber { get; }
 
     /// <summary>
     ///     Determines if the attempt <paramref name="attemptNumber"/> can be retried.
